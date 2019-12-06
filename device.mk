22e8aeca6cf91c87491494de6b90d35f8efe5079
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 14:00:16 -0800

    tundra: Use 6GB dalvik-heap config
    Bug: 145823510
    Test: MPTS
    Change-Id: I086d4a5c09634c3c7bd51ec5d08dcaa1551511b4

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Call the proprietary setup
 $(call inherit-product, vendor/motorola/tundra/tundra-vendor.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)

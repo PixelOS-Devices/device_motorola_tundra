41443e30d68791de265e6dc941ea639419d3d518
raghavt20 <raghavtandon2000@gmail.com>
2021-09-27 01:58:29 +0530

    tundra: Inherit propritary files
    Change-Id: If6e8af0917c6cb58654dd20ff327b9e8176cc06b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,3 +7,6 @@
 BOARD_VENDOR := motorola
 
 DEVICE_PATH := device/motorola/tundra
+
+# Inherit from proprietary files
+include vendor/motorola/tundra/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Call the proprietary setup
+$(call inherit-product, vendor/motorola/tundra/tundra-vendor.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)

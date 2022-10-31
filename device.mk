9dc42f2f292dae024f72beacc826eac8af004d74
SGCMarkus <markusornik@gmail.com>
2022-10-31 18:07:07 +0100

    tundra: Set BOARD_[SHIPPING]_API_LEVEL to 31
    Change-Id: I2d8346b02e2b874e7ea76f4f0a36bf5764ea4a74

--- device.mk
+++ device.mk
@@ -395,7 +395,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_lahaina/android.hardware.sensor.stepdetector.xml \
 
 # Shipping API level
-PRODUCT_SHIPPING_API_LEVEL := 31
+BOARD_API_LEVEL := 31
+BOARD_SHIPPING_API_LEVEL := $(BOARD_API_LEVEL)
+PRODUCT_SHIPPING_API_LEVEL := $(BOARD_API_LEVEL)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)

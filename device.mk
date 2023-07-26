e20f22cf999721ab9d258ddf2d2b0930b6dce920
raghavt20 <raghavtandon2000@gmail.com>
2023-07-27 05:24:57 +0530

    tundra: Copy verified boot permissions
    Change-Id: Id6465086cf0b518557fb8c4c277324b32cc5da3b

--- device.mk
+++ device.mk
@@ -441,6 +441,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vndservicemanager
 
+# Verified boot
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.software.verified_boot.xml \
+    frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service

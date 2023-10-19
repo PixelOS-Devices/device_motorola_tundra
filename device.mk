27d56ef72d0bc684ac7a715a0fe9912480e3ce9a
LuK1337 <priv.luk@gmail.com>
2023-10-19 23:08:10 +0200

    tundra: Build AIDL NXP NFC HAL
    Change-Id: I6ace3ac149c84d2cb9e2773e7ef5ddbf24c35b92

--- device.mk
+++ device.mk
@@ -311,14 +311,13 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2.vendor \
     com.android.nfc_extras \
     NfcNci \
     SecureElement \
     Tag
 
 PRODUCT_PACKAGES += \
-    android.hardware.nfc_snxxx@1.2-service \
+    android.hardware.nfc-service.nxp \
     android.hardware.secure_element@1.2.vendor \
     libchrome.vendor
 

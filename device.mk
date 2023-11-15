5eddfdbc2fbf610dccf87c6e5c33af6d27eafa7a
raghavt20 <raghavtandon2000@gmail.com>
2023-11-15 16:08:50 +0530

    tundra: Provide sensorservice interface on vendor
    11-15 16:06:22.591 21305 21305 E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:1141 LibMap() dlopen: dlopen failed: library "android.frameworks.sensorservice@1.0.so" not found: needed by
     /vendor/lib64/camera/components/com.mot.node.scene_detect.so in namespace (default), on '/vendor/lib64/camera/components/com.mot.node.scene_detect.so'
    
    Change-Id: I0928a101358069a55d342d82124e1265b00e6fd1

--- device.mk
+++ device.mk
@@ -103,6 +103,7 @@ $(call inherit-product, vendor/motorola/tundra/tundra-vendor.mk)
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
